09389070b39f218472e100f735f78d51fca4ff8f
PhillHenry <phillhenry@gmail.com>
2018-12-28 11:15:53 +0000

    Added oversampling.

--- src/main/scala/uk/co/odinconsultants/data/SamplingFunctions.scala
+++ src/main/scala/uk/co/odinconsultants/data/SamplingFunctions.scala
@@ -2,11 +2,28 @@ package uk.co.odinconsultants.data
 
 object SamplingFunctions {
 
-  def trainTest[T](xs: Seq[Seq[T]], ratio: Double): (Seq[T], Seq[T]) = {
+  type Classified[T] = Seq[Seq[T]]
+
+  def trainTest[T](xs: Classified[T], ratio: Double): (Seq[T], Seq[T]) = {
     val train = xs.flatMap { ys => ys.take((ratio * ys.size).toInt) }
     val test  = xs.flatMap { ys => ys.drop((ratio * ys.size).toInt) }
 
     (train, test)
+  }
+
+
+  def repeating[T](xs: Seq[T], iter: Iterator[T]): Stream[T] = Stream.cons(if (iter.hasNext) iter.next() else xs.head, repeating(xs, if (iter.hasNext) iter else xs.toIterator))
+
+  def oversample[T](xs: Seq[(Seq[T], Double)]): Classified[T] = {
+    xs.map { case (ys, w) =>
+      val desired = (ys.size * w).toInt
+      if (ys.size >= desired)
+        ys
+      else {
+        val padding = repeating(ys, ys.toIterator).take(desired - ys.size)
+        ys ++ padding
+      }
+    }
   }
 
 }
--- src/test/scala/uk/co/odinconsultants/data/SamplingFunctionsSpec.scala
+++ src/test/scala/uk/co/odinconsultants/data/SamplingFunctionsSpec.scala
@@ -9,16 +9,19 @@ class SamplingFunctionsSpec extends WordSpec with Matchers {
 
   import SamplingFunctions._
 
+  trait ClassifiedData {
+    val xs: Seq[String]   = Array.fill(10)("x")
+    val ys: Seq[String]   = Array.fill(100)("y")
+    val zs: Seq[String]   = Array.fill(1000)("z")
+
+    val classified: Seq[Seq[String]]        = Seq(xs, ys, zs)
+  }
+
   def numberOf(xs: Seq[String], x: String): Int = xs.filter(_ == x).size
 
   "splitting" should {
-    "give equal proportions of classes" in {
-      val xs: Seq[String]   = Array.fill(10)("x")
-      val ys: Seq[String]   = Array.fill(100)("y")
-      val zs: Seq[String]   = Array.fill(1000)("z")
+    "give equal proportions of classes" in new ClassifiedData {
       val ratio             = 0.9
-
-      val classified        = Seq(xs, ys, zs)
       val (train, test)     = trainTest(classified, 0.9)
 
       numberOf(test, "x") shouldBe 1
@@ -30,6 +33,33 @@ class SamplingFunctionsSpec extends WordSpec with Matchers {
       numberOf(train, "y") shouldBe 90
       numberOf(train, "z") shouldBe 900
       train should have size 999
+    }
+  }
+
+  "infinite stream" should {
+    "repeat sequences" in {
+      val xs      = (1 to 10)
+      val stream  = repeating(xs, xs.toIterator)
+      val n       = 3
+      val result  = stream.take(xs.size * n)
+      result should have size (n * xs.size)
+      xs.foreach { x =>
+        result.filter(_ == x) should have size n
+      }
+    }
+  }
+
+  "oversampling" should {
+    "increase size of minor class" in new ClassifiedData {
+      val weights:        Seq[Double]                 = Seq(1d, 1d, 10d)
+      val class2Weights:  Seq[(Seq[String], Double)]  = classified.zip(weights)
+      val oversampled                                 = oversample(class2Weights)
+      oversampled.zip(class2Weights).foreach { case (actual, (expected, w)) =>
+        val sample = actual.head
+        withClue(s"sequence of '$sample'") {
+          actual should have size (expected.size * w).toInt
+        }
+      }
     }
   }
 
