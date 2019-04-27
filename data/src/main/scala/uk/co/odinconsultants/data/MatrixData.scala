3caed9824eff40614236a1aa100b9cc1069f0474
PhillHenry <phillhenry@gmail.com>
2019-04-27 15:39:34 +0100

    Random matrix generation.

--- data/src/main/scala/uk/co/odinconsultants/data/MatrixData.scala
+++ data/src/main/scala/uk/co/odinconsultants/data/MatrixData.scala
@@ -1,13 +1,25 @@
 package uk.co.odinconsultants.data
 
+import scala.util.Random
+
 object MatrixData {
 
-  type Range[T] = (T, T)
+  type Range = (Int, Int)
 
-//  type RandFn[T] = RandFn[T] => (T, RandFn[T])
+//  case class RandFn[T](seed: RandFn[T]) {
+//    def next: (T, RandFn[T])
+//  }
 
-  def randomCoords[T](n: Int, ranges: Seq[Range[T]]): Seq[Seq[T]] = {
-    ???
+  def randomWith(r: Range, random: Random): Int = {
+    val diff = r._2 - r._1
+    (random.nextDouble() * diff).toInt
+  }
+
+  def randomCoords(n: Int, ranges: Seq[Range], random: Random): Seq[Seq[Int]] = {
+    (1 to n).map { _ =>
+      ranges.map(r => randomWith(r, random))
+    }
+
   }
 
 }
--- data/src/test/scala/uk/co/odinconsultants/data/MatrixDataSpec.scala
+++ data/src/test/scala/uk/co/odinconsultants/data/MatrixDataSpec.scala
@@ -2,18 +2,32 @@ package uk.co.odinconsultants.data
 
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.util.Random
+
 class MatrixDataSpec extends WordSpec with Matchers {
 
   import MatrixData._
+
+  val random = new Random()
 
   "Random coordinates" should {
     val n       = 100
     val range1  = (1, 100)
     val range2  = (100, 200)
     val ranges  = Seq(range1, range2)
-    val coords  = randomCoords(n, ranges)
+
+
+    def checkWithinRange(x: Int, r: (Int, Int)): Unit = {
+      x should be >= r._1
+    }
 
     "be within range" in {
+      val x = randomWith(range1, random)
+      checkWithinRange(x, range1)
+    }
+
+    "be within ranges" in {
+      val coords  = randomCoords(n, ranges, random)
       coords.foreach(xs =>
         xs should have length ranges.length
       )
